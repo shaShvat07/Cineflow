72a47a4489b785e3630cab202d58dcbfbd6ded62
Shashvat <shashvatpatel2803@gmail.com>
2023-09-29 23:23:45 +0530

    redux setup

--- src/components/Movies/Movies.jsx
+++ src/components/Movies/Movies.jsx
@@ -1,8 +1,16 @@
 /* eslint-disable no-console */
 import React from 'react';
+// import { Box, CircularProgress, useMediaQuery, Typography } from '@mui/material';
+// import { useSelector } from 'react-redux';
+
+import { useGetMoviesQuery } from '../../services/TMDB';
 
 const Movies = () => {
   console.log('Movies');
+  const { data } = useGetMoviesQuery();
+  console.log(data);
+
+  // console.log(data);
   return (
     <div>Movies</div>
   );
--- src/index.js
+++ src/index.js
@@ -4,16 +4,19 @@ import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
 // importing mui themes
 import { createTheme, ThemeProvider } from '@mui/material/styles';
-
+import { Provider } from 'react-redux';
 import App from './components/App.jsx';
+import store from './app/store';
 
 const theme = createTheme({});
 // eslint-disable-next-line react/no-deprecated
 ReactDOM.render(
-  <ThemeProvider theme={theme}>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
-  </ThemeProvider>,
+  <Provider store={store}>
+    <ThemeProvider theme={theme}>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </ThemeProvider>
+  </Provider>,
   document.getElementById('root'),
 );
