3d04dde2519980dcc62fdeebcfbc91385be5ec48
Shashvat <shashvatpatel2803@gmail.com>
2024-03-10 20:53:22 +0530

    auth fix

--- src/utils/index.js
+++ src/utils/index.js
@@ -13,7 +13,7 @@ export const fetchToken = async () => {
     const token = data.request_token;
     if (data.success) {
       localStorage.setItem('request_token', token);
-      window.location.href = `https://www.themoviedb.org/authenticate/${token}?redirect_to=https://${window.location.origin}/approved`;
+      window.location.href = `https://www.themoviedb.org/authenticate/${token}?redirect_to=${window.location.origin}/approved`;
     }
   } catch (error) {
     console.log('Sorry Your token could not be created', error);
